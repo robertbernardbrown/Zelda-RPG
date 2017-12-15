c8ae26178fb22db5a697ef40e08a2dfbf3546e3a
Bob Brown <robertbernardbrown@gmail.com>
2017-12-15 14:21:19 -0500

    minor clean up. Removing unnecessary starter comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -145,33 +145,3 @@ $(document).ready(function(){
     
     render(heroes);
 });
-
-// 1. onclick function - user picks which hero they want to be
-// when clicked, hero selection space dissappears - change div class on the chosen hero to the YOUR HERO section
-// defenders switch to Enemies available to attack area - change div class to the enemy section
-
-// 2. onclick function - user picks which defender to play
-// switch class of chosen defender to defender section
-
-// 3. onclick function - attack
-// onclick, run the function to display "you attacked X for X damage, X attacked you for X damage"
-
-// 4. run function to increment the attack power for the hero
-
-// 5. function to run when hitpoints for hero reach 0
-// display loser message and show restart button
-
-// 6. restart button
-// reset the div classes to how they were at the start
-// reset switch locks
-
-// 7. function to run when hitpoints for defender reach 0
-// display winner message and ask them to choose another defender to fight
-// change the old defender class to hidden
-// change the new choice to defender
-
-// 8. function to run when all three defenders are beaten
-// display win message
-// show restart button
-
-// set a variable to incrment whenever a defender is beaten? listen for the third win?
