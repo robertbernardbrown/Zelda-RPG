fdd883d6f38d8b9a13b2b3810b675e509a2b3205
Bob Brown <robertbernardbrown@gmail.com>
2017-12-15 16:08:20 -0500

    adds semicolons where needed in JS. Fixes reset function to not reload entire window but reset globals and DOM elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,21 +20,21 @@ $(document).ready(function(){
     function render (object) {
         for (i in object) {
             if (!object.hasOwnProperty(i)) continue; {
-        $('.row').prepend('<div class="col-xs-6 col-md-3 hero thumbnail"' + 'id = "' + object[i].name + '">')
+        $('.row').prepend('<div class="col-xs-6 col-md-3 hero thumbnail"' + 'id = "' + object[i].name + '">');
         $('#' + object[i].name).prepend('<h1>' + object[i].name + '</h1>')
                   .append(object[i].image)
                   .append('<h2>' + object[i].hitPoints + '</h2>')
                   .attr('attack', object[i].attackPoints)
                   .attr('life', object[i].hitPoints);
-        $('.row').append('</div>')
+        $('.row').append('</div>');
                     }
                 }
-                $('.your-hero').append('<h2> Your hero: </h2>')
-                $('.enemies').append('<h2> Enemies available to attack: </h2>')
-                $('.fight').prepend('<h2> Fight Section </h2>')
-                $('.fight').append('<div class = "btn btn-primary attack">Attack</div>')
-                $('.fight').append('<div class = "fight-text"></div>')
-                $('.defender').append('<h2> Defender: </h2>')
+                $('.your-hero').append('<h2> Your hero: </h2>');
+                $('.enemies').append('<h2> Enemies available to attack: </h2>');
+                $('.fight').prepend('<h2> Fight Section </h2>');
+                $('.fight').append('<div class = "btn btn-primary attack">Attack</div>');
+                $('.fight').append('<div class = "fight-text"></div>');
+                $('.defender').append('<h2> Defender: </h2>');
             }
 
     function chooseHero () {
@@ -62,8 +62,8 @@ $(document).ready(function(){
             isBattle = true;
             villain();
             $('.fight-text').empty();
-            $('.fight-text').append('<div class = "fight-text-hero"></div>')
-            $('.fight-text').append('<div class = "fight-text-villain"></div>')
+            $('.fight-text').append('<div class = "fight-text-hero"></div>');
+            $('.fight-text').append('<div class = "fight-text-villain"></div>');
         }
     }
     
@@ -88,33 +88,33 @@ $(document).ready(function(){
     var counter = 0
     function fight () {
         if (isBattle) {
-            $('.fight-text-hero').empty()
-            $('.fight-text-villain').empty()
+            $('.fight-text-hero').empty();
+            $('.fight-text-villain').empty();
                 if (champion().hitPoints > 0 && champion().hitPoints > villain().attackPoints && villain().hitPoints > 0 && villain().hitPoints > champion().attackPoints) {
-                    villain().hitPoints -= champion().attackPoints
-                    champion().hitPoints -= villain().attackPoints
-                    $('.fight-text-hero').html('<h2> You attacked ' + villain().name + ' for ' + champion().attackPoints + ' hitpoints. </h2>')
+                    villain().hitPoints -= champion().attackPoints;
+                    champion().hitPoints -= villain().attackPoints;
+                    $('.fight-text-hero').html('<h2> You attacked ' + villain().name + ' for ' + champion().attackPoints + ' hitpoints. </h2>');
                     $('.champion h2').html(champion().hitPoints);
-                    $('.fight-text-villain').html('<h2>' + villain().name + ' attacked you for ' + villain().attackPoints + ' hitpoints. </h2>')
+                    $('.fight-text-villain').html('<h2>' + villain().name + ' attacked you for ' + villain().attackPoints + ' hitpoints. </h2>');
                     $('.villain h2').html(villain().hitPoints);
                 }
                 else if (champion().hitPoints <= villain().attackPoints) {
-                    champion().hitPoints -= villain().attackPoints
+                    champion().hitPoints -= villain().attackPoints;
                     $('.champion h2').html(champion().hitPoints);
-                    $('.fight-text-villain').html('<h2>' + villain().name + ' attacked you for ' + villain().attackPoints + ' hitpoints. </h2>')
+                    $('.fight-text-villain').html('<h2>' + villain().name + ' attacked you for ' + villain().attackPoints + ' hitpoints. </h2>');
                 }
                 else if (villain().hitPoints <= champion().attackPoints) {
-                    villain().hitPoints -= champion().attackPoints
+                    villain().hitPoints -= champion().attackPoints;
                     $('.villain h2').html(villain().hitPoints);
-                    $('.fight-text-hero').html('<h2> You attacked ' + villain().name + ' for ' + champion().attackPoints + ' hitpoints. </h2>')
+                    $('.fight-text-hero').html('<h2> You attacked ' + villain().name + ' for ' + champion().attackPoints + ' hitpoints. </h2>');
                 }
 
             counter++
             if (counter === 1) {
-                champion().attackPoints = champion().attackPoints + (champion().attackPoints)
+                champion().attackPoints = champion().attackPoints + (champion().attackPoints);
             }
             else if (counter > 1) {
-                champion().attackPoints = champion().attackPoints + (champion().attackPoints / counter)
+                champion().attackPoints = champion().attackPoints + (champion().attackPoints / counter);
             }
 
             if (champion().hitPoints <= 0) {
@@ -122,37 +122,54 @@ $(document).ready(function(){
             }
             else if (villain().hitPoints <= 0) {
                 chooseAnotherHero();
-                isBattle = false
+                isBattle = false;
             }
         }
     }
 
-    var winCounter = 0
+    var winCounter = 0;
     function chooseAnotherHero () {
-        winCounter++
+        winCounter++;
         if (winCounter < (Object.keys(heroes).length - 1)) {
-        $('.fight-text').append('<h3> You defeated ' + villain().name + '! Choose the next defender to attack. </h3>')
+        $('.fight-text').append('<h3> You defeated ' + villain().name + '! Choose the next defender to attack. </h3>');
         $('#' + villain().name).detach();
         isDefender = true;
         }
         else if (winCounter === (Object.keys(heroes).length - 1)) {
-            win()
+            win();
         }
     }
 
     function win () {
-        $('.fight-text').append('<h3> You defeated ' + villain().name + '! You won! The Triforce is yours') 
+        $('.fight-text').append('<h3> You defeated ' + villain().name + '! You won! The Triforce is yours. Play again?');
         $('#' + villain().name).detach();
+        $('.fight-text').append('<div class = "btn btn-primary reset">Start Over</div>');
     }
 
     function lose () {
-        $('.fight-text').append('<h3> You lost! Try Again? </h3>')
-        $('.fight-text').append('<div class = "btn btn-primary reset">Start Over</div>')
+        $('.fight-text').append('<h3> You lost! Try Again? </h3>');
+        $('.fight-text').append('<div class = "btn btn-primary reset">Start Over</div>');
         var isBattle = false; 
     }
 
     function reset () {
-        window.location.reload();
+        isHero = true;
+        isDefender = false;
+        isBattle = false; 
+        $('.row').empty();
+        $('.your-hero').empty();
+        $('.enemies').empty();
+        $('.fight').empty();
+        $('.defender').empty();
+        heroes = {   
+            hero_1: new MakeHero ('Link', 120, 8, '<img src="assets/images/link.png" alt="link png">'),
+            hero_2: new MakeHero ('Zelda', 100, 8, '<img src="assets/images/zelda.png" alt="zelda png">'),
+            hero_3: new MakeHero ('Dark-Link', 100, 8, '<img src="assets/images/dark-link.png" alt="dark link png">'),
+            hero_4: new MakeHero ('Ganondorf', 150, 8, '<img src="assets/images/ganondorf.png" alt="ganondorf png">')
+            };
+        counter = 0;
+        winCounter = 0;
+        render(heroes);
     }
 
 
